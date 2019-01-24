bcc028e6e23bf98b9bd08238ebe50f1441cd2c49
Luciano Baraúna <baraunaluciano@gmail.com>
2019-01-24 21:51:02 -0200

    finzalizando exemplo foreach

---
+++ ex-code/foreach/exemplo01.php
@@ -0,0 +1,10 @@
+<?php
+   $meses = array(
+       'Janeiro', 'Fevereiro', 'Marco'
+    );
+
+    foreach ($meses as $index => $mes) {
+        echo "Indice: ".$index."<br>";
+        echo "O mês é: ".$mes."<br>";
+    }
+?>
