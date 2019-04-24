cd2af219c9bd18fb941a80d760453d7af52a310c
Luciano Baraúna <baraunaluciano@gmail.com>
2019-04-24 17:23:07 -0300

    terminada aula de slim

--- ex-code/website/index.php
+++ ex-code/website/index.php
@@ -1,5 +1,26 @@
 <?php 
 
-    phpinfo();
+    require_once("vendor/autoload.php");
+
+    // Iniciando o slim pelo namespace da class
+    $app = new \Slim\Slim();
+
+    // Criando uma rota com o get com o parâmetro name
+    // $app->get('/', function ()
+    // {
+    //     echo "Home page ", json_encode(array(
+    //         'date'=>date("Y-m-d H:i:s")
+    //     ));
+    // });
+
+    // Criando uma rota com o get com o parâmetro name
+    $app->get('/hello/:name', function ($name)
+    {
+        echo "Hello, " . $name;
+    });
+
+    // Depois de declarar tudo executamos o run.
+    $app->run();
+
 
 ?>
